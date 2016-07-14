911707cc7ae81c96c1dacb3d3907e5e3a419b079
bhavnamanocha <bhavnamanocha4@gmail.com>
2016-07-14 19:41:48 +0530

    testing pmd

--- Test/src/com/test/Test.java
+++ Test/src/com/test/Test.java
@@ -1,7 +1,7 @@
 package com.test;
 
 public class Test {
-	private String name;
+	//private String name;
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("hello world");
