3120a97e9199dea2776a67b8b3578239b77e0404
bhavnamanocha <bhavnamanocha4@gmail.com>
2016-07-14 19:36:57 +0530

    pmd validation

--- Test/src/com/test/Test.java
+++ Test/src/com/test/Test.java
@@ -1,12 +1,12 @@
 package com.test;
 
 public class Test {
-	
+	private String name;
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("hello world");
 		System.out.println("Hi Hello");
-		String name;
+		
 	}
 
 }
